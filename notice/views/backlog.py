05deb2feb64afdcc42a4e8fc6690c5d11706edee
rey-8lab <caozhengping@8lab.cn>
2022-04-27 14:11:48 +0800

    v0.1.6: frame

---
+++ notice/views/backlog.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+"""
+@Summary : backlog
+@Author  : Rey
+@Time    : 2022-04-27 13:25:25
+"""
+# check if undo backlog exist: resp={'undo': false}
+
+# create a backlog
+
+# list backlog
+
+# finish backlog
