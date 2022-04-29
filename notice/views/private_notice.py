dff432a6fc4375e08c44a510958fc6c4feda804d
rey-8lab <caozhengping@8lab.cn>
2022-04-29 10:31:16 +0800

    v0.1.6

--- notice/views/private_notice.py
+++ notice/views/private_notice.py
@@ -9,7 +9,6 @@ import math
 from django.utils import timezone
 from django.http import JsonResponse, HttpRequest
 from django.db.models import Q
-from django.http.request import QueryDict
 from django.views.decorators.http import require_http_methods
 
 from notice.forms import PrivateForm
@@ -18,10 +17,10 @@ from notice.response import AuthFailed, NotFound, ValidationFailed, ValidationFa
 
 
 # check if exist unread private notice: resp={'undo': false}
-def unread_private(receiver):
+def unread_private(receiver: str):
     filter_params = {
         'is_read': False,
-        "receiver": receiver
+        'receiver': receiver
     }
 
     is_read = PrivateNotice.objects.filter(**filter_params).exists()
@@ -35,7 +34,7 @@ def create_private(data: dict, creator: str):
         return ValidationFailed(f.errors)
 
     data = f.cleaned_data
-    data["creator"] = creator
+    data['creator'] = creator
     private_obj = PrivateNotice.objects.create(**data)
     return JsonResponse(data={'id': private_obj.pk})
 
@@ -45,7 +44,7 @@ def private(request: HttpRequest):
     if not request.user.is_authenticated:
         return AuthFailed()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.POST.dict()
         return create_private(data, request.user.pk)
 
@@ -54,11 +53,9 @@ def private(request: HttpRequest):
 
 
 # list private notice
-def list_private(params: QueryDict, receiver: str):
-    keyword = params.get("keyword", "")
-    queryset = PrivateNotice.objects.filter(
-        Q(title__contains=keyword) | Q(creator__contains=keyword), receiver=receiver
-    )
+def list_private(params: dict, receiver: str):
+    keyword = params.get('keyword')
+
     page = params.get('page', '1')
     if not page.isdigit():
         return ValidationFailed(ValidationFailedDetailEnum.PAGE.value)
@@ -69,31 +66,38 @@ def list_private(params: QueryDict, receiver: str):
         return ValidationFailed(ValidationFailedDetailEnum.SIZE.value)
     size = int(size)
 
+    queryset = PrivateNotice.objects.filter(receiver=receiver)
+    if keyword:
+        queryset = queryset.filter(Q(title__contains=keyword) | Q(creator__contains=keyword))
+
     total = queryset.count()
     max_page = math.ceil(total / size)
 
-    item = list(queryset.order_by("-id").values())[(page - 1) * size: page * size] if page <= max_page else []
+    if page > max_page:
+        items = []
+    else:
+        items = list(queryset.order_by('-id')[(page-1)*size: page*size].values())
+
     resp = {
         'total': total,
         'max_page': max_page,
         'page': page,
-        'items': item
+        'items': items
     }
     return JsonResponse(data=resp)
 
 
-@require_http_methods(["GET"])
+@require_http_methods(['GET'])
 def privates(request: HttpRequest):
-    # if not request.user.is_authenticated:
-    #     return AuthFailed()
+    if not request.user.is_authenticated:
+        return AuthFailed()
     param = request.GET
-    # return list_private(param, request.user.pk)
-    return list_private(param, 1)
+    return list_private(param, str(request.user.pk))
 
 
 # get a private notice detail
-def private_detail(pk: int):
-    private = PrivateNotice.objects.filter(pk=pk).values().first()
+def private_detail(receiver: str, pk: int):
+    private = PrivateNotice.objects.filter(pk=pk, receiver=receiver).values().first()
     if not private:
         return NotFound()
 
@@ -106,13 +110,12 @@ def private_notice_detail(request: HttpRequest, pk: int):
     if not request.user.is_authenticated:
         return AuthFailed()
 
-    return private_detail(pk)
+    return private_detail(str(request.user.pk), pk)
 
 
 # finish private
-def finish_private(pk: int):
-    PrivateNotice.objects.filter(id=pk).update(is_read=True, read_at=timezone.now())
-
+def finish_private(receiver: str, pk: int):
+    PrivateNotice.objects.filter(receiver=receiver, id=pk).update(is_read=True, read_at=timezone.now())
     return JsonResponse(data={})
 
 
@@ -120,4 +123,4 @@ def finish_private(pk: int):
 def f_private(request: HttpRequest, pk: int):
     if not request.user.is_authenticated:
         return AuthFailed()
-    return finish_private(pk)
+    return finish_private(str(request.user.pk), pk)
